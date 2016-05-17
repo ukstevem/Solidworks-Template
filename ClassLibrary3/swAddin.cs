99bf43d8dfbe65c4326615c2d3b6f17587c4fd4b
ukstevem <Stephen.Makepeace@780HWW1>
2016-05-17 15:26:32 +0100

    Add project files.

---
+++ ClassLibrary3/swAddin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SldWorks;
+using SWPublished;
+using SwConst;
+using SwCommands;
+using SolidWorksTools;
+using SolidWorksTools.File;
+
+namespace ClassLibrary3
+{
+    public class Class1
+    {
+    }
+}
